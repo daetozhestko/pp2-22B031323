df3cba2a130d26e7b85e92859360e57c263da9b4
Aldiyar Nurushev <nurushev.aldiyar@gmail.com>
2023-02-21 00:06:59 +0600

    minor

--- tsis4/json/json_task.py
+++ tsis4/json/json_task.py
@@ -3,6 +3,6 @@ s=open('C:/Users/nurus/Desktop/tsis/tsis4/json/sample_data.json','r')
 okay=s.read()
 obj=json.loads(okay)
 i=0
-for i in range(0,18):
+for i in range(len(obj["imdata"])):
     print(obj["imdata"][i]['l1PhysIf']["attributes"]["dn"])
     i+=1
